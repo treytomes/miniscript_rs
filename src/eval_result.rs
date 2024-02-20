facc5357bcad406c0e67ac30181e33f0c738a29f
Trey Tomes <george.tomes@cityelectricsupply.com>
2024-02-20 10:01:34 -0600

    Null is a type.

--- src/eval_result.rs
+++ src/eval_result.rs
@@ -2,6 +2,7 @@ use std::fmt::{Display, Formatter};
 
 #[derive(Debug, PartialEq)]
 pub enum EvalResult {
+    Null,
     Number(f64),
     String(String),
 }
@@ -9,6 +10,7 @@ pub enum EvalResult {
 impl Display for EvalResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
+            EvalResult::Null => write!(f, "null"),
             EvalResult::Number(n) => write!(f, "{}", n),
             EvalResult::String(s) => write!(f, "{}", s),
         }
